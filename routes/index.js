54120fc79d66f5ad55cfedfc2acb3770187c9288
Fernando Ortega 1988 <526o@fresnou.org>
2023-09-15 09:29:05 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/card', function(req,res){
+  console.log(req.body.dateOfBirth)
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    addressLine1: req.body.addressLine1,
+    addressLine2: req.body.addressLine2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: createAccountNumber(),
+    //if you create a new Date with no value it automatically uses todays date!
+    currentDate: new Date()
+  })
+})
+
+module.exports = router;
+
+function createAccountNumber(){
+  let accNum = ""
+  for(let i = 0; i<5; i++)
+}
